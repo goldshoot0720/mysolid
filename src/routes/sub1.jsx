82e9deebcb11c9319404a6535ebb61d7d7eec618
goldshoot0720 <goldshoot0720@gmail.com>
2025-05-01 13:24:03 +0800

    mysolid First

---
+++ src/routes/sub1.jsx
@@ -0,0 +1,11 @@
+import { Title } from "@solidjs/meta";
+
+export default function About() {
+  return (
+    <main>
+      <Title>訂閱資訊管理</Title>
+      <h1>歡迎來到訂閱資訊管理</h1>
+      <Sub></Sub>
+    </main>
+  );
+}
